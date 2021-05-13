87bc4b72ab33b499f2d1aeb90c4477f72be28e79
平石太郎 <t.hiraishi@bravesoft.co.jp>
2021-05-13 19:41:57 +0900

    入力内容を取得する

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,24 @@
 <template>
-  <Editor
-    :apiKey="tinyMCEApiKey"
-    :init="{
-      menubar: false,
-      plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table paste code help wordcount'
-      ],
-    }"
-  />
+  <v-form>
+    <Editor
+      :apiKey="tinyMCEApiKey"
+      v-model="body"
+      :init="{
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount'
+        ],
+      }"
+    />
+
+    <v-btn outlined class="mt-2" @click="save">save</v-btn>
+  </v-form>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from '@vue/composition-api'
+import { computed, defineComponent, ref } from '@vue/composition-api'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Editor from '@tinymce/tinymce-vue'
 
@@ -23,12 +28,20 @@ export default defineComponent({
     Editor,
   },
   setup() {
+    const body = ref('')
+
     const tinyMCEApiKey = computed(() => {
       return process.env.VUE_APP_TINYMCE_API_KEY
     })
 
+    const save = () => {
+      console.log(body.value)
+    }
+
     return {
+      body,
       tinyMCEApiKey,
+      save,
     }
   }
 })
